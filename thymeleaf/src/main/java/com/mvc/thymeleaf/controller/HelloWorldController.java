37ef3f3f131238c30e2f170b23f9a0faf5b7b7d7
Liyuan <xuliyuan1994@yahoo.com>
2023-12-11 13:37:39 +0100

    add data binding 3 versions

--- thymeleaf/src/main/java/com/mvc/thymeleaf/controller/HelloWorldController.java
+++ thymeleaf/src/main/java/com/mvc/thymeleaf/controller/HelloWorldController.java
@@ -1,7 +1,17 @@
 package com.mvc.thymeleaf.controller;
 
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties.Request;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
 
 
 @Controller
@@ -9,7 +19,7 @@ public class HelloWorldController {
 	
 	// need a controller method to show initial HTML form
 	
-	@RequestMapping("/showForm")
+	@GetMapping("/showForm")
 	public String showForm() {
 		
 		return "helloWorld-form";
@@ -23,5 +33,34 @@ public class HelloWorldController {
 		return "helloWorld";
 	}
 	
+	
+	
+	// need a controller method to read form data and add data to the model, binds data in the html
+	@RequestMapping("/processFormTwo")
+	public String processFormTwo(HttpServletRequest request, Model model) {
+		//read the request parameter from the HTML form
+		String theName=request.getParameter("studentName");
+		//change the name
+		theName="Cool!"+theName;
+		//create a message for html page
+		String message="Wellcome "+theName + " ";
+		//add message to the Model
+		model.addAttribute("message", message);
+		
+		return "helloWorld";
+		}
+	
+		// need a controller method: assign Variable theName to value of request parameter
+		@PostMapping("/processFormThree")
+		public String processFormThree(@RequestParam("studentName") String theName, Model model) {
+		
+		theName="Cool!"+theName;
+		//create a message for html page
+		String message="Wellcome"+theName;
+		//add message to the Model
+		model.addAttribute("message", message);
+			
+			return "helloWorld";
+			}
 
 }
